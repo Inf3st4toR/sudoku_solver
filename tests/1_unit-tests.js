204f6e6dd6aa7f3cc3140cc9e4ff94acc5795741
Inf3st4toR 1 <xcharaix@gmail.com>
2025-04-21 07:15:30 +0000

    Completed and Debug Validation function and tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,19 +4,22 @@ const assert = chai.assert;
 const Solver = require('../controllers/sudoku-solver.js');
 const solver = new Solver();
 
-const { testPuzzles } = require('../controllers/puzzles-strings.js');
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js');
+console.log(puzzlesAndSolutions);
 
 suite('Unit Tests', () => {
   test('validate puzzle string of 81 characters', () => {
-    assert.isTrue(solver.validate(testPuzzles[0][0]), "Validation gone wrong.");
+    assert.isTrue(solver.validate(puzzlesAndSolutions[0][0]).isValid, "It should be valid");
   });
 
   test('validate puzzle string of invalid characters ', () => {
-    assert.deepEqual(solver.validate(testPuzzles[5][0]), { error: 'Invalid characters in puzzle' }, "Validation gone wrong.");
+    assert.isFalse(solver.validate(puzzlesAndSolutions[5][0]).isValid, "It should be invalid");
+    assert.equal(solver.validate(puzzlesAndSolutions[5][0]).error, "Invalid characters in puzzle", "Error in the error return");
   });
 
   test('validate puzzle string of not 81 characters ', () => {
-    assert.deepEqual(solver.validate(testPuzzles[6][0]), { error: 'Invalid characters in puzzle' }, "Validation gone wrong.");
+    assert.isFalse(solver.validate(puzzlesAndSolutions[6][0]).isValid, "It should be invalid");
+    assert.equal(solver.validate(puzzlesAndSolutions[6][0]).error, "Expected puzzle to be 81 characters long", "Error in the error return");
   });
 });
 
