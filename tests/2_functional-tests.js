84a3c6f726989ef007ffd51aa3d875fefbe78301
Inf3st4toR 1 <xcharaix@gmail.com>
2025-04-23 04:16:37 +0000

    All puzzle solve features completed and test pass

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,12 +18,51 @@ suite('Functional Tests', () => {
       done();
     });
   });
+
+  test('Solve a invalid puzzle: missing string', (done) => {
+    chai.request(server)
+    .post('/api/solve')
+    .end((err, res) => {
+      assert.equal(res.status, 200, 'res.status is wrong');
+      assert.deepEqual(res.body, { error: 'Required field missing' }, 'Returned object is wrong');
+      done();
+    });
+  });
+
+  test('Solve a invalid puzzle: invalid characters', (done) => {
+    chai.request(server)
+    .post('/api/solve')
+    .send({ puzzle: puzzlesAndSolutions[5][0] })
+    .end((err, res) => {
+      assert.equal(res.status, 200, 'res.status is wrong');
+      assert.deepEqual(res.body, { error: 'Invalid characters in puzzle' }, 'Returned object is wrong');
+      done();
+    });
+  });
+
+  test('Solve a invalid puzzle: invalid length', (done) => {
+    chai.request(server)
+    .post('/api/solve')
+    .send({ puzzle: puzzlesAndSolutions[6][0] })
+    .end((err, res) => {
+      assert.equal(res.status, 200, 'res.status is wrong');
+      assert.deepEqual(res.body, { error: 'Expected puzzle to be 81 characters long' }, 'Returned object is wrong');
+      done();
+    });
+  })
+
+  test('Solve a invalid puzzle: unsolvable', (done) => {
+    chai.request(server)
+    .post('/api/solve')
+    .send({ puzzle: puzzlesAndSolutions[7][0] })
+    .end((err, res) => {
+      assert.equal(res.status, 200, 'res.status is wrong');
+      assert.deepEqual(res.body, { error: 'Puzzle cannot be solved' }, 'Returned object is wrong');
+      done();
+    });
+  })
 });
 
 /*
-Solve a puzzle with valid puzzle string: POST request to /api/solve
-Solve a puzzle with missing puzzle string: POST request to /api/solve
-Solve a puzzle with invalid characters: POST request to /api/solve
-Solve a puzzle with incorrect length: POST request to /api/solve
-Solve a puzzle that cannot be solved: POST request to /api/solve
+
 */
